4b1313761bfac1cc014ca6d84dc236a1c7e318dc
Khushi <khushitiwari1237@gmail.com>
2026-04-22 23:14:53 +0530

    deployment setup

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: process.env.CLIENT_URL || "http://localhost:5173", // frontend URL
   credentials: true
 }));
 
